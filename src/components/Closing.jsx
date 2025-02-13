4f24b42599a2b4ee37d40ad66eb3f296e60ebf03
kennethkeyh <165505239+kennethkeyh@users.noreply.github.com>
2025-02-13 21:55:33 +0700

    Update Closing.jsx valen

--- src/components/Closing.jsx
+++ src/components/Closing.jsx
@@ -32,7 +32,8 @@ function Closing() {
         {/* Greeting text */}
         <div className='w-[90%] max-w-[400px]'>
           <p className="z-10 text-white text-center text-xl font-semibold px-4 drop-shadow-lg">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, delectus? Ut delectus voluptatibus consequatur ex.
+            See you as my valentine’s tomorrow!!! <br/>
+            -6.315355470590679, 106.65210252883588
           </p>
 
         {/* Button */}
